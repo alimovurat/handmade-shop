34ded18db1643e9554f37732d2ac217c0577824a
alimovurat <alimov201253@gmail.com>
2024-03-11 16:44:38 +0300

    feat: Add login modal window

---
+++ main/static/scripts/base.js
@@ -0,0 +1,76 @@
+// открытие формы для регистрации
+
+document.getElementById('openModalBtn').addEventListener('click', function () {
+	document.getElementById('registrationModal').style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function () {
+	document.getElementById('registrationModal').style.display = 'none';
+});
+
+document.getElementsByClassName('close')[1].addEventListener('click', function () {
+	document.getElementById('registrationModal').style.display = 'none';
+});
+
+document.getElementById('registerLink').addEventListener('click', function () {
+	document.getElementById('loginForm').style.display = 'none';
+	document.getElementById('registerForm').style.display = 'block';
+});
+
+document.getElementById('loginLink').addEventListener('click', function () {
+	document.getElementById('registerForm').style.display = 'none';
+	document.getElementById('loginForm').style.display = 'block';
+});
+
+// Проверка пароля
+
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+myInput.onfocus = function () {
+	document.getElementById("message").style.display = "flex";
+}
+
+myInput.onblur = function () {
+	document.getElementById("message").style.display = "none";
+}
+
+myInput.onkeyup = function () {
+	var lowerCaseLetters = /[a-z]/g;
+	if (myInput.value.match(lowerCaseLetters)) {
+		letter.classList.remove("invalid");
+		letter.classList.add("valid");
+	} else {
+		letter.classList.remove("valid");
+		letter.classList.add("invalid");
+	}
+
+	var upperCaseLetters = /[A-Z]/g;
+	if (myInput.value.match(upperCaseLetters)) {
+		capital.classList.remove("invalid");
+		capital.classList.add("valid");
+	} else {
+		capital.classList.remove("valid");
+		capital.classList.add("invalid");
+	}
+
+	var numbers = /[0-9]/g;
+	if (myInput.value.match(numbers)) {
+		number.classList.remove("invalid");
+		number.classList.add("valid");
+	} else {
+		number.classList.remove("valid");
+		number.classList.add("invalid");
+	}
+
+	if (myInput.value.length >= 8) {
+		length.classList.remove("invalid");
+		length.classList.add("valid");
+	} else {
+		length.classList.remove("valid");
+		length.classList.add("invalid");
+	}
+}
--- main/static/scripts/shopping_cart.js
+++ main/static/scripts/shopping_cart.js
@@ -6,3 +6,56 @@ document.getElementById('create-account').addEventListener('change', function ()
 	passwordForm.style.display = this.checked ? 'block' : 'none';
 	passwordConfirmForm.style.display = this.checked ? 'block' : 'none';
 });
+
+// Проверка пароля
+
+var myRegInput = document.getElementById("reg-password");
+var regLetter = document.getElementById("reg-letter");
+var regCapital = document.getElementById("reg-capital");
+var regNumber = document.getElementById("reg-number");
+var regLength = document.getElementById("reg-length");
+
+myRegInput.onfocus = function () {
+	document.getElementById("reg-message").style.display = "flex";
+}
+
+myRegInput.onblur = function () {
+	document.getElementById("reg-message").style.display = "none";
+}
+
+myRegInput.onkeyup = function () {
+	var lowerCaseLetters = /[a-z]/g;
+	if (myRegInput.value.match(lowerCaseLetters)) {
+		regLetter.classList.remove("invalid");
+		regLetter.classList.add("valid");
+	} else {
+		regLetter.classList.remove("valid");
+		regLetter.classList.add("invalid");
+	}
+
+	var upperCaseLetters = /[A-Z]/g;
+	if (myRegInput.value.match(upperCaseLetters)) {
+		regCapital.classList.remove("invalid");
+		regCapital.classList.add("valid");
+	} else {
+		regCapital.classList.remove("valid");
+		regCapital.classList.add("invalid");
+	}
+
+	var numbers = /[0-9]/g;
+	if (myRegInput.value.match(numbers)) {
+		regNumber.classList.remove("invalid");
+		regNumber.classList.add("valid");
+	} else {
+		regNumber.classList.remove("valid");
+		regNumber.classList.add("invalid");
+	}
+
+	if (myRegInput.value.length >= 8) {
+		regLength.classList.remove("invalid");
+		regLength.classList.add("valid");
+	} else {
+		regLength.classList.remove("valid");
+		regLength.classList.add("invalid");
+	}
+}
